196d6b9d80e876704e0a7dd54b1faf16252d9d24
Pravin Sisowath <pravinsisowath@gmail.com>
2020-07-06 02:56:33 -0700

    code

--- app.js
+++ app.js
@@ -1,176 +1,128 @@
 const inquirer = require("inquirer")
 const fs = require("fs")
+const Employee = require("./lib/Employee")
 const Engineer = require("./lib/engineer")
 const Intern = require("./lib/intern")
 const Manager = require("./lib/manager")
 
-const employees = []
-
-function initApp() {
-    startHtml()
-    addMember()
-}
-
-function addMember() {
-    inquirer.prompt([{
-        message: "Enter team member's name",
-        name: "name"
-    },
-    {
-        type: "list",
-        message: "Select team member's role",
-        choices: [
-            "Engineer",
-            "Intern",
-            "Manager"
-        ],
-        name: "role"
-    },
-    {
-        message: "Enter team member's id",
-        name: "id"
-    },
-    {
-        message: "Enter team member's email address",
-        name: "email"
-    }])
-    .then(function({name, role, id, email}) {
-        let roleInfo = ""
-        if (role === "Engineer") {
-            roleInfo = "GitHub username"
-        } else if (role === "Intern") {
-            roleInfo = "school name"
-        } else {
-            roleInfo = "office phone number"
+const team = []
+function start() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: 'Role?',
+            choices: ['Engineer', 'Intern', 'Manager'] 
         }
-        inquirer.prompt([{
-            message: `Enter team member's ${roleInfo}`,
-            name: "roleInfo"
+    ])
+    .then(() => {
+       switch(role) {
+           case 'Employee':
+            Employee()
+           break
+           case 'Engineer':
+            Engineer()
+           break
+           case 'Intern':
+            Intern()
+           break
+           case 'Manager':
+            Manager()
+           break
+       }
+}
+)}
+
+function Employee() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Name?'
         },
         {
-            type: "list",
-            message: "Would you like to add more team members?",
-            choices: [
-                "yes",
-                "no"
-            ],
-            name: "moreMembers"
-        }])
-        .then(function({roleInfo, moreMembers}) {
-            let newMember
-            if (role === "Engineer") {
-                newMember = new Engineer(name, id, email, roleInfo)
-            } else if (role === "Intern") {
-                newMember = new Intern(name, id, email, roleInfo)
-            } else {
-                newMember = new Manager(name, id, email, roleInfo)
-            }
-            employees.push(newMember)
-            addHtml(newMember)
-            .then(function() {
-                if (moreMembers === "yes") {
-                    addMember()
-                } else {
-                    finishHtml()
-                }
-            })
-            
-        })
+            type: 'input',
+            name: 'email',
+            message: 'Email?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'ID?'
+        }
+    ]).then(answers => {
+        let employee = new Employee(answers.name, answers.email, answers.id)
+        team.push(employee)
     })
 }
 
-
-function startHtml() {
-    const html = `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <title>Team Profile</title>
-    </head>
-    <body>
-        <nav class="navbar navbar-dark bg-dark mb-5">
-            <span class="navbar-brand mb-0 h1 w-100 text-center">Team Profile</span>
-        </nav>
-        <div class="container">
-            <div class="row">`
-    fs.writeFile("./output/output.html", html, function(err) {
-        if (err) {
-            console.log(err)
+function Engineer() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Name?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Email?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'ID?'
         }
+    ]).then(answers => {
+        let engineer = new Engineer(answers.name, answers.email, answers.id)
+        team.push(engineer)
     })
-    console.log("start")
 }
 
-function addHtml(member) {
-    return new Promise(function(resolve, reject) {
-        const name = member.getName()
-        const role = member.getRole()
-        const id = member.getId()
-        const email = member.getEmail()
-        let data = ""
-        if (role === "Engineer") {
-            const gitHub = member.getGithub()
-            data = `<div class="col-6">
-            <div class="card mx-auto mb-3" style="width: 18rem">
-            <h5 class="card-header">${name}<br /><br />Engineer</h5>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: ${id}</li>
-                <li class="list-group-item">Email Address: ${email}</li>
-                <li class="list-group-item">GitHub: ${gitHub}</li>
-            </ul>
-            </div>
-        </div>`
-        } else if (role === "Intern") {
-            const school = member.getSchool()
-            data = `<div class="col-6">
-            <div class="card mx-auto mb-3" style="width: 18rem">
-            <h5 class="card-header">${name}<br /><br />Intern</h5>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: ${id}</li>
-                <li class="list-group-item">Email Address: ${email}</li>
-                <li class="list-group-item">School: ${school}</li>
-            </ul>
-            </div>
-        </div>`
-        } else {
-            const officePhone = member.getOfficeNumber()
-            data = `<div class="col-6">
-            <div class="card mx-auto mb-3" style="width: 18rem">
-            <h5 class="card-header">${name}<br /><br />Manager</h5>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: ${id}</li>
-                <li class="list-group-item">Email Address: ${email}</li>
-                <li class="list-group-item">Office Phone: ${officePhone}</li>
-            </ul>
-            </div>
-        </div>`
+function Intern() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Name?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Email?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'ID?'
         }
-        console.log("adding team member")
-        fs.appendFile("./output/output.html", data, function (err) {
-            if (err) {
-                return reject(err)
-            }
-            return resolve()
-        })
-    })  
-}
-
-function finishHtml() {
-    const html = ` </div>
-    </div>
-</body>
-</html>`
-
-    fs.appendFile("./output/output.html", html, function (err) {
-        if (err) {
-            console.log(err)
-        }
+    ]).then(answers => {
+        let intern = new Intern(answers.name, answers.email, answers.id)
+        team.push(intern)
     })
-    console.log("end")
 }
 
+function Manager() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Name?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Email?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'ID?'
+        }
+    ]).then(answers => {
+        let manager = new Manager(answers.name, answers.email, answers.id)
+        team.push(manager)
+    })
+}
 
-initApp()
+start()
